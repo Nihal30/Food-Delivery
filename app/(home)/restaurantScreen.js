8f1c9c83924d804d1612c3d4ce29bd64fc9f5209
Nihal <nihal301298@gmail.com>
2024-04-05 00:27:18 +0530

    Added Restaurant List & Restaurant Screen

---
+++ app/(home)/restaurantScreen.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const restaurantScreen = () => {
+  return (
+    <View>
+      <Text>restaurantScreen</Text>
+    </View>
+  )
+}
+
+export default restaurantScreen
